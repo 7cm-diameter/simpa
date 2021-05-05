8109a09bc934ee40167dd6c17f5ca85b12091da6
7cm-diameter <from.this.ridiculous.loop@gmail.com>
2021-05-05 14:13:35 +0900

    refactoring

--- simpa/simpa.py
+++ simpa/simpa.py
@@ -42,66 +42,77 @@ async def stimulate(agent: Stimulator, expvars: Experimental) -> None:
                 print(f"Trial: {trial}")
                 await agent.sleep(interval)
                 agent.send_to(RECORDER, timestamp(cs_on))
+                agent.send_to(FILMTAKER, HIGH)
                 await agent.call_async(speaker.play, tone)
+                agent.send_to(FILMTAKER, LOW)
                 if trace_interval > 0:
                     await agent.sleep(trace_interval)
                 agent.send_to(RECORDER, timestamp(cs_off))
                 agent.send_to(RECORDER, timestamp(us_on))
-                agent.send_to(FILMTAKER, HIGH)
                 await agent.high_for(us, us_duration)
                 agent.send_to(RECORDER, timestamp(us_off))
-                agent.send_to(FILMTAKER, LOW)
             agent.send_to(OBSERVER, NEND)
+            agent.send_to(RECORDER, NEND)
             agent.finish()
     except NotWorkingError:
         agent.send_to(OBSERVER, ABEND)
+        agent.send_to(RECORDER, ABEND)
     return None
 
 
 class FilmTaker(Agent):
     def __init__(self, addr: str):
         super().__init__(addr)
-        self._led = LOW
+        self._sound = LOW
 
     @property
-    def led(self) -> PinState:
-        return self._led
+    def sound(self) -> PinState:
+        return self._sound
 
 
-async def film(agent: FilmTaker, camid: int, filename: str):
+async def film(agent: FilmTaker, camid: int, filename: str, rec: bool):
     cap = cv2.VideoCapture(camid)
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 400)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 400)
+
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+    cap.set(cv2.CAP_PROP_FPS, 30)
+
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = cap.get(cv2.CAP_PROP_FPS)
-    video = cv2.VideoWriter(filename, fourcc, fps, (width, height))
+
+    if rec:
+        video = cv2.VideoWriter(filename, fourcc, fps, (width, height))
 
     try:
         while agent.working():
-            await agent.sleep(0.01)
-            print(f"{timestamp(None)}")
+            await agent.sleep(0.025)
 
             ret, frame = cap.read()
             if not ret:
                 continue
 
-            if agent.led == HIGH:
+            if agent.sound == HIGH:
                 cv2.circle(frame, (10, 10), 10, (0, 0, 255), thickness=-1)
 
             cv2.imshow(f"Camera: {camid}", frame)
-            video.write(frame)
+            if rec:
+                video.write(frame)
             if cv2.waitKey(1) % 0xFF == ord("q"):
                 break
 
         agent.send_to(OBSERVER, NEND)
+        agent.send_to(RECORDER, timestamp(NEND))
         agent.finish()
     except NotWorkingError:
         agent.send_to(OBSERVER, ABEND)
+        agent.send_to(RECORDER, timestamp(ABEND))
+        agent.finish()
 
     cap.release()
-    video.release()
+    if rec:
+        video.release()
     cv2.destroyAllWindows()
     return None
 
@@ -110,7 +121,7 @@ async def check_pin_state(agent: FilmTaker):
     try:
         while agent.working():
             _, mess = await agent.recv()
-            agent._led = mess
+            agent._sound = mess
     except NotWorkingError:
         pass
     return None
@@ -153,8 +164,9 @@ if __name__ == '__main__':
 
     recorder = Recorder(filename=filename)
 
+    rec = config.experimental.get("video-recording")
     filmtaker = FilmTaker(FILMTAKER) \
-        .assign_task(film, camid=camid, filename=videoname) \
+        .assign_task(film, camid=camid, filename=videoname, rec=rec) \
         .assign_task(check_pin_state) \
         .assign_task(_self_terminate)
 
@@ -162,8 +174,8 @@ if __name__ == '__main__':
 
     agents = [stimulator, reader, recorder, observer, filmtaker]
     register = Register(agents)
-    env_exp = Environment(agents[0:-1])
-    env_cam = Environment([agents[-1]])
+    env_exp = Environment([stimulator, reader, recorder, observer])
+    env_cam = Environment([filmtaker])
 
     try:
         env_cam.parallelize()
